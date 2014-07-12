8ada362109f47b3b33762c0c78bc07263bd3005e
Rob Simpkins <rob.simpkins@crudigital.com.au>
2014-07-12 17:33:02 +1000

    Amend mock POST /api/locations endpoint.

--- www/api/index.php
+++ www/api/index.php
@@ -24,68 +24,7 @@ $app->post('/locations', function() use ($app) {
 		],
 		'currency' => 'aud',
 		'distanceUnit' => 'km',
-		'parkingDuration' => [0, 12],
-		'cheapest' => [
-			[
-				'id' => 2,
-				'street' => 'Wellington Street',
-				'suburb' => 'Woolloongabba',
-				'distance' => [
-					'value' => 12,
-				],
-				'maximumStay' => [
-					'value' => 2,
-					'unit' => 'hr',
-				],
-				'cost' => [
-					'operational' => true,
-					'value' => 1.50,
-				],
-				'spaces' => 12,
-				'restrictions' => [
-					[
-						'days' => 'Mon - Fri',
-						'times' => '10am - 2pm',
-					]
-				],
-				'status' => [
-					'lastReportedFull' => '2014-07-12 11:34:21',
-					'probabilityFull' => 0.2,
-				],
-			],
-			[
-				'id' => 1,
-				'street' => 'Robertson Street',
-				'suburb' => 'Fortitude Valley',
-				'distance' => [
-					'value' => 10,
-				],
-				'maximumStay' => [
-					'value' => 4,
-					'unit' => 'hr',
-				],
-				'cost' => [
-					'operational' => true,
-					'value' => 2.70,
-				],
-				'spaces' => 10,
-				'restrictions' => [
-					[
-						'days' => 'Mon - Fri',
-						'times' => '7am - 7pm',
-					], 
-					[
-						'days' => 'Sat - Sun',
-						'times' => '8am - 2pm',
-					]
-				],
-				'status' => [
-					'lastReportedFull' => '2014-07-12 10:34:21',
-					'probabilityFull' => 0.52,
-				],
-			],
-		],
-		'nearest' => [
+		'locations' => [
 			[
 				'id' => 1,
 				'street' => 'Robertson Street',
